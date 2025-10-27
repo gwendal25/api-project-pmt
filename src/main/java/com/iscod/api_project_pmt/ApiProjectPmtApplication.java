dd9c48ee12dfb7c595f90bc5fc8a7baca4e4f7f7
gwendal <51480637+gwendal25@users.noreply.github.com>
2025-10-27 17:19:58 +0100

    start 🎉 : initial commit

---
+++ src/main/java/com/iscod/api_project_pmt/ApiProjectPmtApplication.java
@@ -0,0 +1,13 @@
+package com.iscod.api_project_pmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjectPmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjectPmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iscod/api_project_pmt/ApiProjectPmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iscod.api_project_pmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectPmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
