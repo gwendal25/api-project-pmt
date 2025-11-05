1d6f061b89b915789fa1fd459e2ef666f24f3c6a
gwendal <51480637+gwendal25@users.noreply.github.com>
2025-11-05 10:59:49 +0100

    feat ✨ : Added get one task route

--- src/main/java/com/iscod/api_project_pmt/controllers/TaskController.java
+++ src/main/java/com/iscod/api_project_pmt/controllers/TaskController.java
@@ -1,12 +1,13 @@
 package com.iscod.api_project_pmt.controllers;
 
+import com.iscod.api_project_pmt.dtos.TaskDto;
+import com.iscod.api_project_pmt.entities.Task;
 import com.iscod.api_project_pmt.mappers.TaskMapper;
 import com.iscod.api_project_pmt.repositories.ProjectRepository;
 import com.iscod.api_project_pmt.repositories.TaskRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -17,5 +18,13 @@ public class TaskController {
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskDto> getTask(@PathVariable Long id) {
+        Task task = taskRepository.findById(id).orElse(null);
+        if(task == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(taskMapper.toDto(task));
+    }
 
 }
